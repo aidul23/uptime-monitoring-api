5ae5bfcbc2c67661b1c4af1dac1f621cf824ed43
Md. Aidul Islam <aidul5613@gmail.com>
2024-01-20 14:38:25 +0200

    create methods for CRUD

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 const routes = require('../routes');
 const {notFoundHandler} = require('../handlers/routeHandlers/notFoundHandler');
 
@@ -20,19 +21,34 @@ handler.handleReqRes = (req, res) => {
         queryStringObject,
         headerObject
     }
+
+    const decoder = new StringDecoder('utf-8')
+    let realData = '';
     
     const chosenHandler = routes[trimedPath] ? routes[trimedPath] : notFoundHandler;
     
-    chosenHandler(requestProperties,(statusCode, payload) => {
-        statusCode = typeof statusCode === 'number' ? statusCode : 500;
-        payload = typeof payload === 'object' ? payload : {};
+    
 
-        const payloadString = JSON.stringify(payload);
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
 
-        //return
-        res.writeHead(statusCode);
-        res.end(payloadString);
-    })
+    req.on('end', () => {
+        realData += decoder.end();
+
+        chosenHandler(requestProperties,(statusCode, payload) => {
+            statusCode = typeof statusCode === 'number' ? statusCode : 500;
+            payload = typeof payload === 'object' ? payload : {};
+    
+            const payloadString = JSON.stringify(payload);
+    
+            //return
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
+        // response handle
+        res.end('Hello world');
+    });
 }
 
 module.exports = handler;
--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const http = require('http');
 const {handleReqRes} = require('./helpers/handleReqRes');
-const env = require('./helpers/environment')
+const env = require('./helpers/environment');
+const data = require('./lib/data');
 
 const app = {};
+
+// testing filesystem
+// @TODO: will clean letter
+
+//create data
+// data.create('test', 'person', {'name': 'iPhone 15', 'price': '1499 euro'}, (err) => {
+//     console.log(`error ${err}`);
+// });
+
+//read data
+// data.read('test','person', (err,data) => {
+//     console.log(err, data);
+// });
+
+//update file
+// data.update('test', 'person',{'name': 'Aidul Islam', 'number': '01679733976'}, (err) => {
+//     console.log(err);
+// });
+
+//delete file
+// data.delete('test', 'per', (err) => {
+//     console.log(err);
+// });
 
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
